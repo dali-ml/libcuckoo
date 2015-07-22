66d47e2184650b2f6f5fa0626c69bbeba949abdf
Szymon Sidor <szymon.sidor@gmail.com>
2015-07-22 00:17:35 -0700

    Libcookoo now compiles on NVCC on Linux

--- src/cuckoohash_map.hh
+++ src/cuckoohash_map.hh
@@ -77,7 +77,7 @@ public:
 
         //! Casting to \p mapped_type runs a find for the stored key. If the
         //! find fails, it will thrown an exception.
-        operator mapped_type() const {
+        operator T() const {
             return owner_.find(key_);
         }
 
@@ -247,7 +247,11 @@ private:
 
         void eraseKV(size_t ind) {
             occupied_.reset(ind);
-            (&kvpair_noconst(ind))->~value_type();
+            #if defined(__CUDACC__) && defined(__linux__)
+                throw std::runtime_error("cookoohash_map::reaseKV - not implemented.");
+            #else
+                (&kvpair_noconst(ind))->value_type::~value_type();
+            #endif
         }
 
         Bucket() {
